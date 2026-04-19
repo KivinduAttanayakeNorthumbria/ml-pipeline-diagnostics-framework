fe0689ce4f3fbb9d907ee2d815e0334d61dc0bc5
Kivindu Attanayake <kivindu.attanayake@northumbria.ac.uk>
2026-04-19 02:05:04 +0100

    Simplify and add image seed variation function

--- src/training_diagnostics/seed_variation.py
+++ src/training_diagnostics/seed_variation.py
@@ -4,8 +4,8 @@ import torch
 import numpy as np
 from src.utils.performance_tracker import PerformanceTracker
 
-def run_seed_variation(train_function, evaluate_function, X_train, y_train, X_test, y_test, config, is_pytorch = False):
-    print("Stage 2: Seed Variation experiment")
+def run_seed_variation_tab(train_function, evaluate_function, X_train, y_train, X_test, y_test, config, is_pytorch = False):
+    print("Stage 2: Seed variation experiment for tabular")
 
     seeds = config['random_seeds']['seed_variation_list']
     num_seeds = config['stage2_training_stability']['seed_variation']['num_seeds']
@@ -20,25 +20,29 @@ def run_seed_variation(train_function, evaluate_function, X_train, y_train, X_te
         if is_pytorch:
             torch.manual_seed(seed)
 
+            # Train model
             tracker.start_performance_track()
             model = train_function(X_train, y_train, config)
             tracker.stop_performance_track(f"Training completed for seed: {seed}")
 
+            # Evaluation
             tracker.start_performance_track()
-            accuracy, y_pred = evaluate_function(model, X_test, y_test, config)
+            accuracy, y_prediction = evaluate_function(model, X_test, y_test, config)
             tracker.stop_performance_track(f"Evaluating completed for seed: {seed}")
 
         else:
+            # Train model
             tracker.start_performance_track()
             model = train_function(X_train, y_train, config)
             tracker.stop_performance_track(f"Training completed for seed: {seed}")
 
+            # Evaluation
             tracker.start_performance_track()
-            accuracy, y_pred = evaluate_function(model, X_test, y_test)
+            accuracy, y_prediction = evaluate_function(model, X_test, y_test)
             tracker.stop_performance_track(f"Evaluating completed for seed: {seed}")
 
         all_accuracies.append(accuracy)
-        all_predictions.append(y_pred)
+        all_predictions.append(y_prediction)
 
     # Calculate stability metrics
     accuracies = np.array(all_accuracies)
@@ -57,5 +61,51 @@ def run_seed_variation(train_function, evaluate_function, X_train, y_train, X_te
     else:
         print("Low variance, model stable")
 
-    print("Stage 2: Seed Variation experiment completed.")
+    print("Stage 2: Seed variation experiment for tabular completed.")
+    return results
+
+def run_seed_variation_img(train_function, evaluate_function, train_set, test_set, config):
+    print("Stage 2: Seed variation experiment for images")
+
+    seeds = config['random_seeds']['image_seed_list']
+    num_seeds = len(seeds)
+    tracker = PerformanceTracker()
+
+    all_accuracies = []
+    all_predictions = []
+
+    for seed in seeds[:num_seeds]:
+        torch.manual_seed(seed)
+        np.random.seed(seed)
+
+        # Train model
+        tracker.start_performance_track()
+        model = train_function(train_set, config)
+        tracker.stop_performance_track(f"Training completed for seed: {seed}")
+
+        # Evaluation
+        tracker.start_performance_track()
+        accuracy, y_prediction = evaluate_function(model, test_set, config)
+        tracker.stop_performance_track(f"Evaluating completed for seed: {seed}")
+
+        all_accuracies.append(accuracy)
+        all_predictions.append(y_prediction)
+
+    accuracies = np.array(all_accuracies)
+    results = {
+        'accuracy': accuracies,
+        'mean_accuracy': round(float(accuracies.mean()), 4),
+        'std_accuracy': round(float(accuracies.std()), 4),
+        'min_accuracy': round(float(accuracies.min()), 4),
+        'max_accuracy': round(float(accuracies.max()), 4),
+        'predictions': all_predictions,
+        'performance': tracker.get_results()
+    }
+
+    if results['std_accuracy'] > 0.02:
+        print("High variance, model unstable")
+    else:
+        print("Low variance, model stable")
+
+    print("Stage 2: Seed variation experiment for images completed.")
     return results
